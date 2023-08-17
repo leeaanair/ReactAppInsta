b49bc9fef79424547a043da735c91b11642b41af
leeaanair <leeaanair@gmail.com>
2023-08-18 00:32:16 +0530

    Added sideDrawer, transition

--- src/shared/components/Navigations/MainNavigation.js
+++ src/shared/components/Navigations/MainNavigation.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
 	
+	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+	
+	const openDrawerHandler = () => {
+		
+		setDrawerIsOpen(true);
+	}
+	
+	const closeDrawerHandler = () => {
+		
+		setDrawerIsOpen(false);
+	}
+	
 	return(
 		<React.Fragment>
-			<SideDrawer className = "main-navigation__drawer-nav">
-				<NavLinks />
-			</SideDrawer>
+		{
+			drawerIsOpen && <Backdrop  onClick={closeDrawerHandler}/>
+		}
+		
+		{ 
+/* 			drawerIsOpen ? (
+				<SideDrawer className = "main-navigation__drawer-nav">
+					<NavLinks />
+				</SideDrawer>
+		
+			) : null
+ */		
+ 				<SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+				<nav className = "main-navigation__drawer-nav">
+					<NavLinks />
+				</nav>
+				</SideDrawer>
+
+ 
+		}
 			<MainHeader>
 			{/* 		This goes in props.children of MainHeader
 			*/	}		
-				<button className = "main-navigation__menu-btn">
+				<button className = "main-navigation__menu-btn" onClick={openDrawerHandler}>
 					<span />
 					<span />
 					<span />
