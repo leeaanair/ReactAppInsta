6fbceb176d34896de39adee58cee15fb3ec96fb8
leeaanair <leeaanair@gmail.com>
2023-09-19 00:44:31 +0530

    Added form with title and added validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+
 
 const NewPlace = () => {
 	return (
-	<h2> NewPlace work! </h2>
+	<form className="place-form">
+		<Input element = "input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid value"/>
+	</form>
 	);
 }
 
